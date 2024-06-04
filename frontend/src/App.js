318724266b4f9a7ed8cb5df1ea2a6b0088449099
Balvardhan Parihar <68677559+BALVARDHAN@users.noreply.github.com>
2024-06-04 10:53:01 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,7 @@ import MiddlePanel from "./components/MiddlePanel";
 import OrderPanel from "./components/OrderPanel";
 
 function App() {
-  const auth_token =
-    "eyJ0eXAiOiJKV1QiLCJrZXlfaWQiOiJza192MS4wIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiJBVTA0MDgiLCJqdGkiOiI2NjVkODI0ODA1M2FiYjBlY2YxMDNjMzciLCJpc011bHRpQ2xpZW50IjpmYWxzZSwiaWF0IjoxNzE3NDA0MjMyLCJpc3MiOiJ1ZGFwaS1nYXRld2F5LXNlcnZpY2UiLCJleHAiOjE3MTc0NTIwMDB9.qzg5Qpfq3DGvOE8kfha5pfxenKc45L-HZ10ugXmGL9Y";
+  const auth_token = "";
 
   return (
     <div className="bg-slate-700 h-full flex items-stretch gap-3 p-6">
